ad71c2727d4b75cf31baccc641d2d4ef058712fe
petruciucur <petruciucur@yahoo.com>
2024-02-15 14:19:35 +0200

    Added pet fallback image

--- main.js
+++ main.js
@@ -1,42 +1,45 @@
-const template = document.querySelector("#pet-card-template");
-const wrapper = document.createDocumentFragment();
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
 
 async function start() {
-  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
-  const weatherData = await weatherPromise.json();
-  // console.log(weatherData.properties.periods[0].temperature);
-  const ourTemperature = weatherData.properties.periods[0].temperature;
-  const celsius = (ourTemperature - 32) * 5 / 9;
-  document.querySelector("#temperature-output").textContent = ourTemperature;
-  document.querySelector("#grade-celsius").textContent = celsius.toFixed(2);
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
+
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperature-output").textContent = ourTemperature
+
 }
 
-start();
+start()
 
 async function petsArea() {
-  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
-  const petsData = await petsPromise.json();
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
   petsData.forEach(pet => {
-    const clone = template.content.cloneNode(true);
-    clone.querySelector("h3").textContent = pet.name;
-    clone.querySelector(".pet-description").textContent = pet.description;
-    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear);
-    clone.querySelector(".pet-card-photo img").src = pet.photo;
-    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`;
-    wrapper.appendChild(clone);
+    const clone = template.content.cloneNode(true)
 
+    clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+
+    if (!pet.photo) pet.photo = "images/fallback.jpg"
+
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
+
+    wrapper.appendChild(clone)
   })
-  document.querySelector(".list-of-pets").appendChild(wrapper);
+  document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
-petsArea();
+petsArea()
 
 function createAgeText(birthYear) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - birthYear;
+  const currentYear = new Date().getFullYear()
+  const age = currentYear - birthYear
 
-  if (age == 1) return "1 year old";
-  if (age == 0) return "Less than a year old";
+  if (age == 1) return "1 year old"
+  if (age == 0) return "Less than a year old"
 
-  return `${age} years old`;
+  return `${age} years old`
 }
